066c8c35364e1f1ceb4a1bb9e82a982b5a5b6768
Tiphaine JUQUEL <Tiphaine.juquel@gmail.com>
2023-04-03 12:46:06 +0200

    Initial commit

---
+++ Exercices/Exo1.js
@@ -0,0 +1,28 @@
+//### 01 Exercice
+//
+// 1. Créer un petit jeu en console : on doit deviner un nombre compris entre 1 et 100. Si l'utilisateur propose un nombre plus petit on lui indique qui l'est plus grand et réciproquement.
+//
+// 2. L'utilisateur à 10 tentatives pour deviner le nombre caché, après le jeu s'arrête. Si l'utilisateur trouve le nombre avant cette borne, le jeu s'arrête également.
+//
+// 3. Pensez à gérer également les erreurs de saisi dans le jeu.
+
+const num = Math.floor(Math.random() * 100) + 1;
+const essai = 0;
+
+function Game() {
+    const choose = document.form1.devine1.value;
+    essai++;
+    const status = "Nombre d'essais : " + essai;
+    if (choose < num)
+        document.form1.indice.value = "Non, le nombre est plus grand.";
+    if (choose > num)
+        document.form1.indice.value = "Non, le nombre est plus petit.";
+    if (choose == num) {
+        window.alert("Correct ! Vous avez trouvé en " + essai + "essais.");
+        location.reload();
+    }
+    if (essai == 10) {
+        window.alert("Désolé, c'est fini. Le nombre correct était : " + num);
+        location.reload();
+    }
+}
---
+++ demo.js
@@ -0,0 +1,8 @@
+console.table({
+    name: "Node.JS",
+    gestionnaire_paquet: "npm"
+});
+
+console.log('Hello Node.js');
+
+//dans le terminal, tapez node demo.js pour voir si le tableau s'affiche.
