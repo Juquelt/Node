df845cbd2fdb1251202436a9f96f8fc5611edc8b
Tiphaine JUQUEL <Tiphaine.juquel@gmail.com>
2023-04-04 10:24:48 +0200

    commit refractor

---
+++ Jour-2/Exercices/Exo1/customer/index.js
@@ -0,0 +1,9 @@
+const u = require('utils');
+
+//import
+
+const priceHT = [
+    { name : "Apple", priceHT : 1.0, priceTTC : 2.00 },
+    { name : "Orange", priceHT : 1.2, priceTTC : 2.00 },
+    { name : "Rasberry", priceHT : 2.5, priceTTC : 2.00 },
+];
